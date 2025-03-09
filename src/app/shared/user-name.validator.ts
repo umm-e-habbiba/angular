4b27ce0d5bcfbc5bbed04f8adca4c62f38828731
umm-e-habiba <umehabiba0305@gmail.com>
2025-03-09 07:25:06 +0500

    conditional validations

--- src/app/shared/user-name.validator.ts
+++ src/app/shared/user-name.validator.ts
@@ -7,7 +7,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 //   const forbidden = /admin/.test(control.value);
 //   return forbidden ? { forbiddenName: { value: control.value } } : null;
 // }
-//for string from function parameter , we will create a facory function which will return validatorfunction
+//for string from function parameter , we will create a factory function which will return validatorfunction
 export function forbiddenNameValidator(forbiddenName: RegExp): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     const forbidden = forbiddenName.test(control.value);
--- src/app/views/reactive/reactive.component.ts
+++ src/app/views/reactive/reactive.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -15,8 +15,10 @@ import { passwordValidator } from '../../shared/password-validator';
   templateUrl: './reactive.component.html',
   styleUrl: './reactive.component.scss',
 })
-export class ReactiveComponent {
+export class ReactiveComponent implements OnInit {
   // constructor(private formBuilder: FormBuilder) {}
+
+  regForm!: FormGroup;
 
   get password() {
     return this.regForm.get('password');
@@ -25,23 +27,43 @@ export class ReactiveComponent {
   get confirmPassword() {
     return this.regForm.get('confirmPassword');
   }
+  //confirmPassword
+  get email() {
+    return this.regForm.get('email');
+  }
   formBuilder = inject(FormBuilder);
   // formControls via formBuilder service
-  regForm = this.formBuilder.group(
-    {
-      userName: [
-        'ume habiba',
-        [Validators.required, forbiddenNameValidator(/admin/)],
-      ], //in array first one will be the default value, second will be the validation rules
-      password: ['', [Validators.required, Validators.minLength(5)]], // for multiple validation rules we add array in second value
-      confirmPassword: ['', Validators.required],
-      address: this.formBuilder.group({
-        city: [''],
-        country: [''],
-      }),
-    },
-    { validator: passwordValidator }
-  );
+  ngOnInit(): void {
+    this.regForm = this.formBuilder.group(
+      {
+        userName: [
+          'ume habiba',
+          [Validators.required, forbiddenNameValidator(/admin/)], // custom validation
+        ], //in array first one will be the default value, second will be the validation rules
+        password: ['', [Validators.required, Validators.minLength(5)]], // for multiple validation rules we add array in second value
+        confirmPassword: ['', Validators.required], // simple validation
+        email: [''],
+        subscribe: [false],
+        address: this.formBuilder.group({
+          city: [''],
+          country: [''],
+        }),
+      },
+      { validator: passwordValidator } //cross-field validation
+    );
+
+    //conditional validation
+    //valueChanges of every control return an observable so we will subscribe to it
+    this.regForm.get('subscribe')?.valueChanges.subscribe((checked) => {
+      const email = this.regForm.get('email');
+      if (checked) {
+        email?.setValidators(Validators.required);
+      } else {
+        email?.clearValidators();
+      }
+      email?.updateValueAndValidity();
+    });
+  }
 
   // regForm = new FormGroup({
   //   userName: new FormControl('ume'),
@@ -65,8 +87,8 @@ export class ReactiveComponent {
     this.regForm.patchValue({
       // if you don't want to pass all values, then you have to use patchValue
       userName: 'ume',
-      password: 'xyz',
-      confirmPassword: 'xyz',
+      password: '12345',
+      confirmPassword: '12345',
       // address: {
       //   city: 'Taxila',
       //   country: 'Pakistan',
